e124d2d6ae17403894c5a95a13cb7e6003f3fd66
tiago-silva-batista <tiagosbsantos@outlook.comm>
2025-04-21 18:38:39 -0300

    aplicando tema dark e finalizando o projeto

---
+++ src/themes/light.ts
@@ -0,0 +1,9 @@
+const temaLight = {
+  corPrincipal: '#282035',
+  corSecundaria: '#949494',
+  corDeFundo: '#fff',
+  corDeFundoBotao: '#4476bf',
+  corDaBorda: '#c1c1c1,'
+}
+
+export default temaLight
